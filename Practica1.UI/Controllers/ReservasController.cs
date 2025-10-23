3a48112118fe838fdffd0b46f9cbc39875de9e74
Sebastian Vasquez <svasvil0211@gmail.com>
2025-10-23 17:30:02 -0600

    Reserva Administrativa Done , falta el modulo de Crud de Reserva  | Revisar el editar Habitacion.

---
+++ Practica1.UI/Controllers/ReservasController.cs
@@ -0,0 +1,108 @@
+﻿using Practica1.Abstracciones.LogicaNegocio.Reservas.ObtenerReservas;
+using Practica1.Abstracciones.ModelosUI.Reservas;
+using Practica1.LogicaNegocio.Reservas.ObtenerReserva;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica1.UI.Controllers
+{
+    public class ReservasController : Controller
+    {
+        private readonly IObtenerReservaLN _obtenerReservaLN;
+
+
+        public ReservasController()
+        {
+            _obtenerReservaLN = new ObtenerReservasLN();
+        }   
+        // GET: Reservas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reservas/Details/5
+        public ActionResult ListaReservasHabitacion(int? idHabitacion) // Observa el '?'
+        {
+            // Debes manejar el caso donde idHabitacion sea null
+            if (!idHabitacion.HasValue)
+            {
+                // Redirigir, mostrar error o mostrar una lista vacía/completa
+                return View("ListaReservasHabitacion", new List<ReservacionesDTO>());
+            }
+
+            // Usar .Value para obtener el int subyacente
+            List<ReservacionesDTO> lista = _obtenerReservaLN.Obtener(idHabitacion.Value);
+            return View("ListaReservasHabitacion", lista);
+        }
+
+        // GET: Reservas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservas/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reservas/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reservas/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reservas/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reservas/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
