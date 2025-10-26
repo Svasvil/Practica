c0b4f541c245516161cfa6b69e18fe5904a5a400
Sebastian Vasquez <svasvil0211@gmail.com>
2025-10-25 19:50:17 -0600

    CambiosDone terminados

--- Practica1.AccesoDat/AccesoDatos/Habitaciones/ObtenerHabitacionID_AD/ObtenerHabitacionID.cs
+++ Practica1.AccesoDat/AccesoDatos/Habitaciones/ObtenerHabitacionID_AD/ObtenerHabitacionID.cs
@@ -14,33 +14,29 @@ namespace Practica1.AccesoDat.AccesoDatos.Habitaciones.ObtenerHabitacionID_AD
             objetoContexto = new ObjetoContexto();
         }
 
-        public async Task<HabitacionDTO> Obtener(int id)
+        public Task<HabitacionDTO> Obtener(int id)
         {
-            HabitacionDTO habitacion = await Task.Run(() =>
-                (from h in objetoContexto.HABITACIONES
-                 where h.ID == id
-                 select new HabitacionDTO
-                 {
-                     ID = h.ID,
-                     Codigo = h.Codigo,
-                     Nombre = h.Nombre,
-                     Ubicacion = h.Ubicacion,
-                     CantidadHuespedesAdmitidos = h.CantidadHuespedesAdmitidos,
-                     CantidadCamas = h.CantidadCamas,
-                     CantidadBanos = h.CantidadBanos,
-                     ResponsableLimpieza = h.ResponsableLimpieza,
-                     CostoLimpieza = h.CostoLimpieza,
-                     CostoReserva = h.CostoReserva,
-                     TipoHabitacion = h.TipoHabitacion,
-                     Estado = h.Estado,
-                     FechaRegistro = h.FechaRegistro,
-                     FechaModificacion = h.FechaModificacion
-                 }).FirstOrDefault()
-            );
+            HabitacionDTO habitacion = (from h in objetoContexto.HABITACIONES
+                                        where h.ID == id
+                                        select new HabitacionDTO
+                                        {
+                                            ID = h.ID,
+                                            Codigo = h.Codigo,
+                                            Nombre = h.Nombre,
+                                            Ubicacion = h.Ubicacion,
+                                            CantidadHuespedesAdmitidos = h.CantidadHuespedesAdmitidos,
+                                            CantidadCamas = h.CantidadCamas,
+                                            CantidadBanos = h.CantidadBanos,
+                                            ResponsableLimpieza = h.ResponsableLimpieza,
+                                            CostoLimpieza = h.CostoLimpieza,
+                                            CostoReserva = h.CostoReserva,
+                                            TipoHabitacion = h.TipoHabitacion,
+                                            Estado = h.Estado,
+                                            FechaRegistro = h.FechaRegistro,
+                                            FechaModificacion = h.FechaModificacion
+                                        }).FirstOrDefault();
 
-           
-
-            return habitacion;
+            return Task.FromResult(habitacion);
         }
     }
 }
