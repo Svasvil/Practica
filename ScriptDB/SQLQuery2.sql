8cb53155fb87d17f814b2588173b8a67efc404e4
Sebastian Vasquez <svasvil0211@gmail.com>
2025-11-29 23:35:04 -0600

    Rename database and create new tables for reservations
    db update

--- ScriptDB/SQLQuery2.sql
+++ ScriptDB/SQLQuery2.sql
@@ -1,62 +1,172 @@
 
-CREATE DATABASE Habitaciones
+CREATE DATABASE CASO_PRACTICO_RESERVACIONES
+
 GO
 
-USE Habitaciones;
+USE CASO_PRACTICO_RESERVACIONES
+
 GO
 
-
-drop table InfoHabitaciones;
-CREATE TABLE InfoHabitaciones (
- 
-    ID INT PRIMARY KEY IDENTITY (1,1),
-    CODIGO INT NOT NULL UNIQUE, 
-    NOMBRE VARCHAR(100) NOT NULL,
-    UBICACION VARCHAR(150) NOT NULL,
-    CANTIDAD_HUESPEDES_PERMITIDOS INT NOT NULL,
-    CANTIDAD_DE_CAMAS INT NOT NULL,
-    CANTIDAD_DE_BANIOS INT NOT NULL,
-    RESPONSABLE_DE_LIMPIEZA VARCHAR(100),
-    COSTO_DE_LIMPIEZA DECIMAL(10, 2) NOT NULL,
-    COSTO_DE_RESERVA DECIMAL(10, 2) NOT NULL,
-    TIPO_DE_HABITACION INT NOT NULL,
-    FECHA_DE_REGISTRO DATETIME NOT NULL,
-    FECHA_DE_MODIFICACION DATETIME,
-    ESTADO INT  NOT NULL 
+CREATE TABLE HABITACIONES (
+    Id int identity (1,1) not null,
+    CodigoDeHabitacion varchar(7) not null,
+    NombreDeHabitacion varchar(30) not null,
+	CantidadDeHuespedesPermitidos int not null,
+	CantidadDeCamas int not null,
+	CantidadDeBanos int not null,
+	Ubicacion varchar(10) not null,
+	EncargadoDeLimpieza varchar(100) not null,
+	TipoDeHabitacion int not null,
+	CostoDeLimpieza decimal(18,2) not null,
+	CostoDeReserva decimal(18,2) not null,
+	FechaDeRegistro datetime not null,
+	FechaDeModificacion datetime,
+	Estado bit,
 );
+
+select * from RESERVACIONES;
+go
+
+CREATE TABLE RESERVACIONES (
+    Id int identity (1,1) not null,
+    NombreDeLaPersona varchar(150) not null,
+    Identificacion varchar(30) not null,
+	Telefono varchar(10) not null,
+	Correo varchar(50) not null,
+	FechaNacimiento datetime not null,
+	Direccion varchar(200) not null,
+	MontoTotal decimal(18,2) not null,
+	FechaInicioReserva datetime not null,
+	FechaFinReserva datetime not null,
+	FechaDeRegistro datetime not null,
+	IdHabitacion int not null,
+);
+
+/*Tablas de identity para roles.*/
+
+
+/****** Object:  Table [dbo].[AspNetRoles]    Script Date: 12/11/2024 13:29:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetRoles](
+	[Id] [nvarchar](128) NOT NULL,
+	[Name] [nvarchar](256) NOT NULL,
+CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserClaims]    Script Date: 12/11/2024 13:29:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUserClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [nvarchar](128) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+CONSTRAINT [PK_dbo.AspNetUserClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserLogins]    Script Date: 12/11/2024 13:29:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUserLogins](
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[ProviderKey] [nvarchar](128) NOT NULL,
+	[UserId] [nvarchar](128) NOT NULL,
+CONSTRAINT [PK_dbo.AspNetUserLogins] PRIMARY KEY CLUSTERED 
+(
+	[LoginProvider] ASC,
+	[ProviderKey] ASC,
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserRoles]    Script Date: 12/11/2024 13:29:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUserRoles](
+	[UserId] [nvarchar](128) NOT NULL,
+	[RoleId] [nvarchar](128) NOT NULL,
+CONSTRAINT [PK_dbo.AspNetUserRoles] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUsers]    Script Date: 12/11/2024 13:29:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUsers](
+	[Id] [nvarchar](128) NOT NULL,
+	[Email] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEndDateUtc] [datetime] NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
+	[UserName] [nvarchar](256) NOT NULL,
+CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AspNetUserClaims]  WITH CHECK ADD  CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserClaims] CHECK CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId]
+GO
+ALTER TABLE [dbo].[AspNetUserLogins]  WITH CHECK ADD  CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserLogins] CHECK CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId]
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[AspNetRoles] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId]
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId]
 GO
 
----------------------------------------------------
 
-INSERT INTO [dbo].InfoHabitaciones
-           ([CODIGO]
-           ,[NOMBRE]
-           ,[UBICACION]
-           ,[CANTIDAD_HUESPEDES_PERMITIDOS]
-           ,[CANTIDAD_DE_CAMAS]
-           ,[CANTIDAD_DE_BANIOS]
-           ,[RESPONSABLE_DE_LIMPIEZA]
-           ,[COSTO_DE_LIMPIEZA]
-           ,[COSTO_DE_RESERVA]
-           ,[TIPO_DE_HABITACION]
-           ,[FECHA_DE_REGISTRO]
-           ,[FECHA_DE_MODIFICACION]
-           ,[ESTADO])
-     VALUES
-           (1111
-           ,'Habitación Junior Vista al Jardín'
-           ,'Piso 1, Ala Oeste'
-           ,2
-           ,1
-           ,1
-           ,'Ana López'
-           ,15.00
-           ,80.00
-           ,1 -- Junior
-           ,GETDATE()
-           ,NULL
-           ,1)
-GO
--- 5. Consulta de los Datos Insertados
-SELECT * FROM InfoHabitaciones
-GO
+/*Vemos los usuarios y roles */
+
+select* from ASPNETUSERS;
+select *from AspNetRoles;
+
+
+/*Creamos los roles.*/
+
+insert into ASPNETROLES values (NEWID(), 'Admin')
+insert into ASPNETROLES values (NEWID(), 'User')
+
+/*Vemos el rol y el usuario*/
+ select * from AspNetUserRoles
